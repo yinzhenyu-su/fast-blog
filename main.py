9390208848d5ae5bed8bf42db41901705fb14523
nunts <sharkrain@outlook.com>
2021-04-21 23:52:15 +0800

    add time

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from datetime import datetime
 
 app = FastAPI()
+
+class Time():
+    time = ''
+    def __init__(self, time: str):
+        self.time = time
 
 @app.get('/')
 def root_get():
     return {
         'message': 'hello world!'
     }
+
+@app.get('/time')
+def time_get():
+    return ORJSONResponse(Time(datetime.now().strftime('%Y-%m-%d %H:%M:%S %f')).__dict__)
 
 if __name__=='__main__':
     import uvicorn
